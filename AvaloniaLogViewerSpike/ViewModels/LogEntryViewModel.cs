31e84a9145fd122e062a0dfd99fbc28cdcac13ba
Antony Scott <antony.scott@bybox.com>
2020-02-20 23:54:33 +0000

    add font/colour styling

--- AvaloniaLogViewerSpike/ViewModels/LogEntryViewModel.cs
+++ AvaloniaLogViewerSpike/ViewModels/LogEntryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avalonia.Media;
+using System;
 
 namespace AvaloniaLogViewerSpike.ViewModels
 {
@@ -7,5 +8,16 @@ namespace AvaloniaLogViewerSpike.ViewModels
         public DateTime? Timestamp { get; set; }
         public string Severity { get; set; }
         public string Message { get; set; }
+
+        public SolidColorBrush Colour => Severity.ToLower() switch
+            {
+                "error" => new SolidColorBrush(Colors.IndianRed),
+                "verbose" => new SolidColorBrush(Colors.Gray),
+                "debug" => new SolidColorBrush(Colors.DimGray),
+                "info" => new SolidColorBrush(Colors.LawnGreen),
+                "warning" => new SolidColorBrush(Colors.Orange),
+                "fatal" => new SolidColorBrush(Colors.Red),
+                _ => new SolidColorBrush(Colors.White)
+            };
     }
 }
--- AvaloniaLogViewerSpike/ViewModels/LogViewModel.cs
+++ AvaloniaLogViewerSpike/ViewModels/LogViewModel.cs
@@ -1,9 +1,11 @@
-﻿using AvaloniaLogViewerSpike.Messages;
+﻿using Avalonia.Media;
+using AvaloniaLogViewerSpike.Messages;
 using Dock.Model.Controls;
 using Newtonsoft.Json;
 using ReactiveUI;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Linq;
 
 namespace AvaloniaLogViewerSpike.ViewModels
@@ -12,6 +14,12 @@ namespace AvaloniaLogViewerSpike.ViewModels
     {
         [JsonProperty]
         public string Name { get; set; } = null;
+
+        [JsonIgnore]
+        public int FontSize { get; set; } = 20;
+
+        [JsonIgnore]
+        public FontFamily FontFamily { get; set; } = new FontFamily("Courgette");
 
         IDisposable _messageSubscriber = null;
         IDisposable _logEntrySubscriber = null;
@@ -50,17 +58,20 @@ namespace AvaloniaLogViewerSpike.ViewModels
                     _messageSubscriber = observer
                         .Subscribe(logEntriesMessage =>
                         {
-                            var logEntriesObserver = logEntriesMessage
-                                .LogEntries
-                                .ToObservable()
-                                .ObserveOn(RxApp.MainThreadScheduler);
+                            if (logEntriesMessage?.LogEntries?.Any() ?? false)
+                            {
+                                var logEntriesObserver = logEntriesMessage
+                                    .LogEntries
+                                    .ToObservable()
+                                    .ObserveOn(RxApp.MainThreadScheduler);
 
-                            // TODO: this will change based on filters set in the UI (ie - debug only / message contains "x" / etc)
-                            //logEntriesObserver = logEntriesObserver
-                            //    .Where(x => x.Severity == "Debug");
+                                // TODO: this will change based on filters set in the UI (ie - debug only / message contains "x" / etc)
+                                //logEntriesObserver = logEntriesObserver
+                                //    .Where(x => x.Severity == "Debug");
 
-                            _logEntrySubscriber?.Dispose();
-                            _logEntrySubscriber = logEntriesObserver.Subscribe(LogEntries.Add);
+                                _logEntrySubscriber?.Dispose();
+                                _logEntrySubscriber = logEntriesObserver.Subscribe(LogEntries.Add);
+                            }
                         });
                 });
         }
